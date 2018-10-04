8a855e31b4ca846b6cca8ecac153c7d2d2d18b1c
Brian Muller <bamuller@gmail.com>
2018-10-04 09:39:44 -0400

    added long description to package

--- aioipfs_api/__init__.py
+++ aioipfs_api/__init__.py
@@ -1,4 +1,4 @@
 """
 Python 3 async client for interacting with the IPFS HTTP API
 """
-__version__ = "0.1.1"
+__version__ = "0.1.3"
--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@
 from setuptools import setup, find_packages
 import aioipfs_api
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="aioipfs-api",
     version=aioipfs_api.__version__,
     description="Python 3 async client for interacting with the IPFS HTTP API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Brian Muller",
     author_email="bamuller@gmail.com",
     license="MIT",
